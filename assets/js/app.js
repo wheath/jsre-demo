f68ba0f5c18c24e031cf5871104cc015733f6ea5
wheath <wgheath@gmail.com>
2013-01-08 18:03:31 -1000

    adding simple alert

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,11 +5,12 @@ function onLoad() {
 // Cordova is loaded and it is now safe to make calls Cordova methods
 function onDeviceReady() {
 // Now safe to use the Cordova API
-   var re = setup_re();
-   var q_ram = new Rule('choose_ram');
-   var term_X = new Term('X');
-   q_ram.addArg(term_X);
-   re.fireRule(q_ram);
+   alert('_dbg in onDeviceReady'); 
+   //var re = setup_re();
+   //var q_ram = new Rule('choose_ram');
+   //var term_X = new Term('X');
+   //q_ram.addArg(term_X);
+   //re.fireRule(q_ram);
    
-   re.handleQueryResult(q_ram);
+   //re.handleQueryResult(q_ram);
 }
