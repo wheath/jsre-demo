e66c8f7224e4a747a4af8f75575fb6d2d2a05065
wheath <wgheath@gmail.com>
2013-01-08 19:06:57 -1000

    version 1.0.8 moving to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,13 @@
 
-window.alert = notification.alert;
-document.addEventListener('deviceready', function(){alert('Hi there');}, true);
 
+function onLoad(){
+  document.addEventListener("deviceready", onDeviceReady, true);
+}
+
+function onDeviceReady(){
+  window.alert = navigator.notification.alert;
+  alert("PhoneGap version 1.0.8");
+}
 
 // Cordova is loaded and it is now safe to make calls Cordova methods
 // Now safe to use the Cordova API
