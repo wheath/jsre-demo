95c4c4f3d0c04f452532030289e9d8e38ef5cb0b
wheath <wgheath@gmail.com>
2013-01-08 17:58:53 -1000

    fixing js initialization

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,10 @@
-$(function() {
+function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    }
+
+// Cordova is loaded and it is now safe to make calls Cordova methods
+function onDeviceReady() {
+// Now safe to use the Cordova API
    var re = setup_re();
    var q_ram = new Rule('choose_ram');
    var term_X = new Term('X');
@@ -6,5 +12,4 @@ $(function() {
    re.fireRule(q_ram);
    
    re.handleQueryResult(q_ram);
-});
-                   
+}
